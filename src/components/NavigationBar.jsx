096abc400a6af1fcf59deac95ade33c820443a68
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-09-28 21:51:51 +0100

    add account and sign out links to nav

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,12 +2,10 @@ import { useState, useEffect, useContext } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { SignedInUserContext } from '../context/SignedInUser';
 import { Link } from "react-router-dom";
+import signOutAcc from '../utils/signOutAcc';
 import supabase from '../utils/supabaseClient';
-
-
 
 function NavigationBar() {
     const { signedInUser, setSignedInUser } = useContext(SignedInUserContext);
@@ -44,23 +42,14 @@ function NavigationBar() {
           <Nav className="me-auto">
             <Link to="/">Home</Link>
             <Link to="/eventlist">Events</Link>
-            {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
           <Nav>
-            <Link to="/account">{signedInUser? signedInUser?.user?.email : 'Log in'}</Link>
-            {/* <button onClick={()=>{
-                console.log(signedInUser)
-            }}>TESTS</button> */}
+          { !signedInUser ?  <Link to="/account">Sign In</Link> :
+          <>
+              <Link to="/account">Account</Link> 
+              <Link onClick={signOutAcc}>Sign Out</Link> 
+          </>
+          }
           </Nav>
         </Navbar.Collapse>
       </Container>
