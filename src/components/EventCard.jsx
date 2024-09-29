f2c0a9ae260c1296db0e6f208d683ee784a85850
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-09-29 11:00:08 +0100

    add bootstrap card component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,12 +1,15 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function EventCard() {
+function EventCard({event}) {
+    const url = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+    console.log(event)
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={`${url}/storage/v1/object/public/${event.img_path}`}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{event.title}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
--- src/components/EventCardList.jsx
+++ src/components/EventCardList.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect, useContext } from 'react';
+import fetchEventData from '../utils/fetchEventData';
+import EventCard from './EventCard';
+
 function EventCardList () {
+    const [events, setEvents] = useState([]);
+    
+    useEffect(()=>{
+        fetchEventData().then(({data})=>{
+            setEvents(data)
+            console.log(data)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    },[])
+
     return (<div id="event-list">
         <h1>Event Grid</h1>
-        
+        {
+            events.map((event)=><EventCard key={event.id} event={event}/>)
+        }
     </div>)
 }
 
