a2b516d29a5cbb73527b1a6aa6dad50635166513
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-10-17 01:18:25 +0100

    add tab navigation for user and staff account page

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -4,14 +4,18 @@ import EventForm from '../components/EventForm';
 import PageBanner from '../components/PageBanner';
 import { fetchProfileSignupEvents } from '../utils/fetchProfile';
 import EventCardList from "../components/EventCardList";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 function AccountPage () {
     const { signedInUser } = useContext(SignedInUserContext);
     const [isStaff, setIsStaff] = useState(false);
     const [attendEvents, setAttendEvents] = useState([]);
     const [createdEvents, setCreatedEvents] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(()=> {
+        setIsLoading(true);
         fetchProfileSignupEvents(signedInUser.user.id)
         .then(({data}) => {
             if (data.length > 0) {
@@ -29,38 +33,71 @@ function AccountPage () {
             }
         }).catch((err)=>{
             console.log('ERROR (fetchProfileSignupEvents)', err)
-        })
+        }).finally(()=>{
+            setIsLoading(false);
+        });
     },[]);
 
-    return (
-        <main id="account-page">
-            {
-                isStaff ? 
-                <>
-                    <h1>Staff Account</h1>
+    if(isLoading){
+        return (<main id="account-page"><h1>Loading Account...</h1></main>)
+    } else {
+        if (isStaff){
+            return (<main id="account-page">
+                    <h1>Staff Account</h1> 
+                    <Tabs
+                        defaultActiveKey="Create Event Form"
+                        id="uncontrolled-tab-example"
+                        className="mb-3"
+                        justify
+                    >
+                    <Tab eventKey="Create Event Form" title="Create Event Form">
                     <EventForm signedInUser={signedInUser} />
-                </> :
-                <>
-                    <h1>User Account</h1>
-                </>
-            }
-            {
-                createdEvents.length > 0 ?
-                <EventCardList 
-                    title= {`Created Events`} 
-                    showEventsIds= {createdEvents}
-                    control= {isStaff}
-                /> : null
-            }
-            {
-                attendEvents.length > 0 ?
-                <EventCardList 
-                    title= {`Events To Attend`} 
-                    showEventsIds={attendEvents}
-                /> : null
-            }
-        </main>
-    )
+                    </Tab>
+                    <Tab eventKey="Created Events" title="Created Events">
+                    {
+                        createdEvents.length > 0 ?
+                        <EventCardList 
+                            title= {`Created Events`} 
+                            showEventsIds= {createdEvents}
+                            control= {isStaff}
+                        /> : <h1>You haven't created any events yet!</h1>
+                    }
+                    </Tab>
+                    <Tab eventKey="Events To Attend" title="Events To Attend">
+                        {
+                        attendEvents.length > 0 ?
+                        <EventCardList 
+                            title= {`Events To Attend`} 
+                            showEventsIds={attendEvents}
+                        />
+                        : <h1>You haven't signed up to any events yet!</h1>
+                        }
+                    </Tab> 
+                    </Tabs>
+                </main>)
+        } else {
+            return (<main id="account-page">
+                <h1>User Account</h1> 
+                <Tabs
+                    defaultActiveKey="Events To Attend"
+                    id="uncontrolled-tab-example"
+                    className="mb-3"
+                    justify
+                >
+                <Tab eventKey="Events To Attend" title="Events To Attend">
+                    {
+                    attendEvents.length > 0 ?
+                    <EventCardList 
+                        title= {`Events To Attend`} 
+                        showEventsIds={attendEvents}
+                    />
+                    : <h1>You haven't signed up to any events yet!</h1>
+                    }
+                </Tab> 
+                </Tabs>
+            </main>)
+        }
+    }
 }
 
 export default AccountPage;
